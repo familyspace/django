e2bedada172835a075665b81b8747f91516929af
tum0xa <timofey.samodurov@mail.ru>
2019-03-27 00:02:17 +0500

    Добавлен перевод для контроллера SignIn приложения authapp

--- authapp/forms.py
+++ authapp/forms.py
@@ -9,10 +9,10 @@ class SignInForm(AuthenticationForm):
 
         for field_name, field in self.fields.items():
             field.widget.attrs['required'] = True
-            field.widget.attrs['class'] = "form-control"
+            field.widget.attrs['class'] = 'form-control'
             field.label = ""
 
             if field_name == 'username':
-                field.widget.attrs['placeholder'] = _("Username")
+                field.widget.attrs['placeholder'] = _('Username')
             elif field_name == 'password':
-                field.widget.attrs['placeholder'] = _("Password")
+                field.widget.attrs['placeholder'] = _('Password')
