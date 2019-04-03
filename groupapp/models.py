1596ee589c9249a5f8714e0bc43c70d2d0084332
Rolandinovich <mromanmail@gmail.com>
2019-04-03 12:57:01 +0300

    Переписал модель GroupUser

--- groupapp/models.py
+++ groupapp/models.py
@@ -38,9 +38,9 @@ class GroupUser(models.Model):
     '''
     Таблица списка пользователей в группе
     '''
-    User = models.ForeignKey(User, verbose_name='Пользователь',on_delete='CASCADE')
+    User = models.ForeignKey(User, verbose_name='Пользователь', related_name='usergroups', on_delete='CASCADE')
     role = models.CharField(verbose_name='Роль',
                             max_length=3,
                             choices=[(item, item.value) for item in RoleChoice],
                             default=RoleChoice.ADM)
-    group = models.ForeignKey(Group, related_name='groupusers', on_delete='CASCADE')
+    group = models.ForeignKey(Group, related_name='users', on_delete='CASCADE')
