0c3d3bd6bf663ab7b41828f6adf311b7279ccbf0
Mikhail Petukhov <petmik@yandex.ru>
2019-04-06 17:46:52 +0300

    Занес в модели groupapp ранее сделанное

--- groupapp/models.py
+++ groupapp/models.py
@@ -1,14 +1,29 @@
 from django.db import models
 from enum import Enum
 from authapp.models import User
+from django.shortcuts import get_object_or_404
 
 
 # Create your models here.
+
+def get_groups_list(self):
+    groups_list = Group.objects.all()
+    return groups_list
+
+def create_group(user, title, category_name):
+    my_category = get_object_or_404(Category, category_name=category_name)
+    my_group = Group.objects.create(title=title, category=my_category)
+    my_group.add_user(user)
+    print('Группа ' + title + ' создана')
+    return my_group
+
 class Category(models.Model):
     category_name = models.CharField(verbose_name='Название категории',
                                      max_length=100,
                                      blank=False,
                                      null=False)
+    def __str__(self):
+        return self.category_name
 
 
 class Group(models.Model):
@@ -25,6 +40,31 @@ class Group(models.Model):
                                  verbose_name='Категория группы',
                                  on_delete='PROTECT')
 
+    def __str__(self):
+        return self.title + ' ' + self.category.category_name
+
+    # def get_users(self):
+    #     relations = GroupUser.objects.filter(group=self.pk)
+    #     members = map(lambda item: item.user, relations)
+    #     return members
+
+    def get_users(self):
+        relations = self.groupusers.all()
+        members = map(lambda item: item.user, relations)
+        return members
+
+    def add_user(self, user):
+        GroupUser.objects.create(user=user, group=self)
+        comment = 'Участник добавлен'
+        return comment
+
+    def remove_user(self, user):
+        group_user = get_object_or_404(GroupUser, user=user, group=self)
+        print('юзер опознан')
+        group_user.delete()
+        print('и удален из группы')
+        comment = 'Участник удален'
+        return comment
 
 class RoleChoice(Enum):
     '''
@@ -42,5 +82,11 @@ class GroupUser(models.Model):
     role = models.CharField(verbose_name='Роль',
                             max_length=3,
                             choices=[(item, item.value) for item in RoleChoice],
-                            default=RoleChoice.ADM)
+                            default=RoleChoice.USR)
     group = models.ForeignKey(Group, related_name='groupusers', on_delete='CASCADE')
+
+    class Meta:
+        unique_together = ('user', 'group')
+
+    def __str__(self):
+        return self.user.username + ' ' + self.group.title + ' ' + self.role
